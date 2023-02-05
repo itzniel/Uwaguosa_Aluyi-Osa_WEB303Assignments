aa1a858766bf8bc67744689c58611de219b06a89
itzniel <w0817788@myscc.ca>
2023-02-05 09:12:39 -0500

    On load function
    Added the onload function

--- Assignment 2/js/main.js
+++ Assignment 2/js/main.js
@@ -4,8 +4,11 @@ $(document).ready(function() {
     const xhr = new XMLHttpRequest();
 
     $('#prospect').click(function(){
+        xhr.onload = function (){
+
         document.getElementById('content').innerHTML = xhr.responseText;
-        $("#content").animate({height: "toggle"});
+        $("#content").animate({width: "toggle"});
+    }
         xhr.open("GET", 'prospect.html', true);
         xhr.send();
     })
@@ -13,9 +16,10 @@ $(document).ready(function() {
     
 
     $('#convert').click(function(){
-
+        xhr.onload = function (){
          document.getElementById('content').innerHTML = xhr.responseText;
-         $("#content").animate({height: "toggle"});
+         $("#content").animate({height: "toggle"});}
+
          xhr.open("GET", 'convert.html', true);
          xhr.send();
      })
@@ -23,8 +27,10 @@ $(document).ready(function() {
      
 
      $('#retain').click(function(){
+        xhr.onload = function (){
         document.getElementById('content').innerHTML = xhr.responseText;
          $("#content").animate({height: "toggle"});
+        }
          xhr.open("GET", 'retain.html', true);
          xhr.send();
      })
